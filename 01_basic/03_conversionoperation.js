06884cdd71c06346bea717509732955eaae9ddcf
noob7programmer <a33008480@gmail.com>
2026-05-04 20:35:51 +0000

    conversion operations

---
+++ 01_basic/03_conversionoperation.js
@@ -0,0 +1,28 @@
+//let score = null
+
+//console.log(typeof score);
+
+//let number = Number(score);
+//console.log(number);
+
+
+
+//"33" ==> 33
+//"33abc" ==> NaN
+//true ==>1 ; false==>0
+
+//let isloggedin = true
+
+
+//console.log(isloggedin);
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
+
+
+
+
