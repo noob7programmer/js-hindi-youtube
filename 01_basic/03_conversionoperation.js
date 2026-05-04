da6f4d609675cf934cf1012b46a438a5b0d8b807
noob7programmer <a33008480@gmail.com>
2026-05-04 20:59:35 +0000

    operations

--- 01_basic/03_conversionoperation.js
+++ 01_basic/03_conversionoperation.js
@@ -16,13 +16,36 @@
 
 //console.log(isloggedin);
 
-let somenumber = 33
-let stringnumber = String(somenumber)
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// let somenumber = 33
+// let stringnumber = String(somenumber)
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
 
+// *************************************operation*****************************************************
 
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue);
 
+// // console.log(2+2);
+// // console.log(2-2);
+// // console.log(2*2);
+// // console.log(2**2);
+// // console.log(2/2);
+// // console.log(2%2);
 
+// let str1 = "hello"
+// let str2 = " Aditya"
+// let str3 = str1 + str2
+// console.log(str3);
 
+// console.log(+true);
+// console.log(+"");
 
+// let num1 , num2 , num3 
+
+// num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
